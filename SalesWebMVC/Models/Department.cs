3f15180f95963f10d132ea99bd6473021138f319
GreatestSun <aceleradao@gmail.com>
2021-03-07 20:11:49 -0300

    SellersController

--- SalesWebMVC/Models/Department.cs
+++ SalesWebMVC/Models/Department.cs
@@ -1,6 +1,25 @@
-﻿namespace SalesWebMVC.Models {
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SalesWebMVC.Models {
     public class Department {
         public int ID { get; set; }
         public string Name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+        public Department() { }
+        public Department(int iD, string name) {
+            ID = iD;
+            Name = name;
+        }
+
+        public void AddSeller(Seller seller) {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime from, DateTime to) {
+            return Sellers.Sum(seller => seller.TotalSales(from, to));
+        }
     }
 }
---
+++ SalesWebMVC/Models/Seller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace SalesWebMVC.Models {
+    public class Seller {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double BaseSalary { get; set; }
+        public DateTime BirthDate { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller() { }
+
+        public Seller(int id, string name, string email, double baseSalary, DateTime birthDate, Department department) {
+            Id = id;
+            Name = name;
+            Email = email;
+            BaseSalary = baseSalary;
+            BirthDate = birthDate;
+            Department = department;
+        }
+
+        public void AddSales(SalesRecord sr) {
+            Sales.Add(sr);
+        }
+
+        public void RemoveSales(SalesRecord sr) {
+            Sales.Remove(sr);
+        }
+
+        public double TotalSales(DateTime from, DateTime to) {
+            //Version 1
+            /*
+            double total = 0.0;
+            foreach(SalesRecord record in Sales) {
+                if (record.Date >= from && record.Date <= to) {
+                    total += record.Amount;
+                }
+            }
+            return total;
+            */
+
+            //Version 2
+            return Sales.Where(sr => sr.Date >= from && sr.Date <= to).Sum(sr => sr.Amount);
+        }
+    }
+}
