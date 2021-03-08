cb7002968f9050433941ba20021176f94745d8ae
GreatestSun <aceleradao@gmail.com>
2021-03-08 11:31:11 -0300

    Details view + eager loading

---
+++ SalesWebMVC/Services/SellerService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMVC.Services {
+    public class SellerService {
+        private readonly SalesWebMVCContext _context;
+
+        public SellerService(SalesWebMVCContext context) {
+            _context = context;
+        }
+
+        public Seller FindById(int id) {
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
+        }
+        public List<Seller> FindAll() {
+            return _context.Seller.ToList();
+        }
+
+        public void Insert(Seller seller) {
+            _context.Add(seller);
+            _context.SaveChanges();
+        }
+
+        public void Remove(int id) {
+            Seller seller = _context.Seller.Find(id);
+            _context.Seller.Remove(seller);
+            _context.SaveChanges();
+        }
+    }
+}
